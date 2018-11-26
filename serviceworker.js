5d5da40dbf70de29413ec73f6d7f0787ca8163ec
Matthew Perkins <mattperkinsee@gmail.com>
2018-11-25 19:34:30 -0500

    Updates
    Added: Responsive to restaurant.html, tabindex fixes, serviceworker.js restaurant.html id pages

--- serviceworker.js
+++ serviceworker.js
@@ -5,6 +5,16 @@ self.addEventListener("install", function(event) {
                 './',
                 './index.html',
                 './restaurant.html',
+                './restaurant.html?id=1',
+                './restaurant.html?id=2',
+                './restaurant.html?id=3',
+                './restaurant.html?id=4',
+                './restaurant.html?id=5',
+                './restaurant.html?id=6',
+                './restaurant.html?id=7',
+                './restaurant.html?id=8',
+                './restaurant.html?id=9',
+                './restaurant.html?id=10',
                 './css/styles.css',
                 './js/main.js',
                 './js/restaurant_info.js',
@@ -19,7 +29,6 @@ self.addEventListener("install", function(event) {
                 './img/8.jpg',
                 './img/9.jpg',
                 './img/10.jpg',
-                './img/favicon.ico',
                 './img/logo.svg'
             ]);
         })

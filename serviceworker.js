1606ef480228da8d9e71a4e72c09cf4401f30092
Matthew Perkins <mattperkinsee@gmail.com>
2019-01-23 04:43:39 -0500

    Updates
    Updates: Fixed URL for images to deploy on host

--- serviceworker.js
+++ serviceworker.js
@@ -25,10 +25,10 @@ self.addEventListener("install", function(event) {
                 './img/4.jpg',
                 './img/5.jpg',
                 './img/6.jpg',
-                './img/7.jpg',
-                './img/8.jpg',
-                './img/9.jpg',
-                './img/10.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
                 './img/logo.svg'
             ]);
         })
